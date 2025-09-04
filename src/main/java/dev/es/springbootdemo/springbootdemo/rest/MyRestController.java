575f6785b4588fc7e111e938d5a610c1fa3106ff
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-09-04 11:30:47 +0200

    inject properties

--- src/main/java/dev/es/springbootdemo/springbootdemo/rest/MyRestController.java
+++ src/main/java/dev/es/springbootdemo/springbootdemo/rest/MyRestController.java
@@ -1,11 +1,28 @@
 package dev.es.springbootdemo.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("unused")
 @RestController
 public class MyRestController {
+
+    // inject properties operator.name and operator.group
+
+    @Value("${operator.name}")
+    private String userName;
+
+    @Value("${operator.group}")
+    private String groupName;
+
+    // expose new endpoint for "teminfo"
+
+    @GetMapping("api/teaminfo")
+    public String getTeaminfo(){
+        return "user:" + userName + " group:" + groupName;
+    }
+
     // expose "/api/hello" that return "Hello World"
     @GetMapping("/api/hello")
     public String sayHello(){
