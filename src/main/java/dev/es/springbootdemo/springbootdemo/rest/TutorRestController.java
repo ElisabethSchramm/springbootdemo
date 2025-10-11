18a20d246a1cf42bf7deaa8c6fb5527426885f26
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-12 01:46:41 +0200

    feat(scope): apply prototype scope to JavaTutor and verify bean identity in TutorRestController

--- src/main/java/dev/es/springbootdemo/springbootdemo/rest/TutorRestController.java
+++ src/main/java/dev/es/springbootdemo/springbootdemo/rest/TutorRestController.java
@@ -8,16 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TutorRestController {
-    private Tutor myTutor;
-
+    private final Tutor myTutor;
+    private final Tutor tutor;
     @Autowired
-    public TutorRestController(@Qualifier("javaTutor") Tutor myTutor) {
+    public TutorRestController(@Qualifier("javaTutor") Tutor myTutor,
+                               @Qualifier("javaTutor") Tutor tutor) {
         this.myTutor = myTutor;
+        this.tutor = tutor;
     }
-
 
     @GetMapping("/dailyexercise")
     public String getDailyExercise(){
         return this.myTutor.getDailyExercise();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myTutor == tutor);
     }
 }
