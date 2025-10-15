1792af7fce6e7bf730d593ce106db41d792d434e
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-15 22:34:48 +0200

    Added CommandLineRunner to SpringbootdemoApplication

--- src/main/java/dev/es/springbootdemo/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/dev/es/springbootdemo/springbootdemo/SpringbootdemoApplication.java
@@ -1,7 +1,9 @@
 package dev.es.springbootdemo.springbootdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Spring Boot Demo
@@ -12,5 +14,10 @@ public class SpringbootdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
-
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello world");
+        };
+    }
 }
