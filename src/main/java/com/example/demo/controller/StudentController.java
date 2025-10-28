a3e00498498ca0f45ef7acbfa111b24f5b627d3b
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-28 18:01:24 +0100

    added property programmingLanguages for radiobutton

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -16,12 +16,15 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${programmingLanguages}")
+    private List<String> programmingLanguages;
+
     @GetMapping("/showStudentForm")
     public String showForm (Model model){
 
         model.addAttribute("student", new Student());
-
         model.addAttribute("countries", countries);
+        model.addAttribute("languages", programmingLanguages);
 
         return "student-form";
     }
