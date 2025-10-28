1834b85c2cea044b2b8dd6c7873b03a6494c6db8
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-28 15:53:49 +0100

    feat(form): add letsShout2 with @RequestParam and update form to processForm3

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -26,6 +27,17 @@ public class HelloWorldController {
         String name = request.getParameter("studentName");
         name = name.toUpperCase();
         String result = "YO " + name;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    // read data and add data to the model with @RequestParam
+    @RequestMapping("/processForm3")
+    public String letsShout2(@RequestParam("studentName") String name, Model model) {
+
+        String result = "YO2 " + name.toUpperCase();
 
         model.addAttribute("message", result);
 
