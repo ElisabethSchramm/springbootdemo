2175cb396ecd29b4eb2df010bd135790f27e1fd5
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-28 16:46:51 +0100

    changed @RequestMapping to @GetMapping in HelloWorldController.showForm() and @RequestMapping to @PostMapping HelloWorldController.letsShout2() changed form method in helloworld-form.html from GET to POST

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
 
     // show initial HTML form
-    @RequestMapping("/showFrom")
+    @GetMapping("/showFrom")
     public String showForm() {
         return "helloworld-form";
     }
@@ -25,22 +27,16 @@ public class HelloWorldController {
     @RequestMapping("/processForm2")
     public String letsShout(HttpServletRequest request, Model model) {
         String name = request.getParameter("studentName");
-        name = name.toUpperCase();
-        String result = "YO " + name;
-
+        String result = "YO " + name.toUpperCase();
         model.addAttribute("message", result);
-
         return "helloworld";
     }
 
     // read data and add data to the model with @RequestParam
-    @RequestMapping("/processForm3")
+    @PostMapping("/processForm3")
     public String letsShout2(@RequestParam("studentName") String name, Model model) {
-
         String result = "YO2 " + name.toUpperCase();
-
         model.addAttribute("message", result);
-
         return "helloworld";
     }
 }
