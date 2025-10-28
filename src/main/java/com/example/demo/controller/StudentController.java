a60542786a3904f1c0003fc212ae9962c1538051
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-28 17:11:08 +0100

    feat(student): add Student model, StudentController, form view, and confirmation page

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm (Model model){
+
+        model.addAttribute("student", new Student());
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm (@ModelAttribute("student") Student student){
+
+        return "student-confirmation";
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class Student {
+
+    public String firstName;
+    public String lastName;
+
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
