c9decb866039f8bdde3f8206d35c6c6210504504
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-29 17:15:04 +0100

    renamed application

--- src/main/java/com/example/demo/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/example/demo/thymeleaf/ThymeleafApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo.mvccrud;
+package com.example.demo.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class ThymeleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
+		SpringApplication.run(ThymeleafApplication.class, args);
 	}
 
 }
