828ec72a413e638a5ce67ebf0a44b41d525e56c3
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-29 15:48:51 +0100

    added custom annotation CorseCode for validation

---
+++ src/main/java/com/example/demo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,23 @@
+package com.example.demo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String>  {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String code, ConstraintValidatorContext context) {
+        if(code == null)
+            return true;
+        else
+            return code.startsWith(coursePrefix);
+    }
+}
