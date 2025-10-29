c51e371eb47a4c15a874fc88da32e31aeda5ab12
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-29 21:35:11 +0100

    add functions to save employee

--- src/main/java/com/example/demo/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/example/demo/thymeleaf/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.demo.thymeleaf.controller;
 
+import com.example.demo.thymeleaf.entity.Employee;
 import com.example.demo.thymeleaf.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +24,18 @@ public class EmployeeController {
     @GetMapping("/list")
     public String listEmployees(Model model){
         model.addAttribute("employees", employeeService.findAll());
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        model.addAttribute("employee", new Employee());
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 }
