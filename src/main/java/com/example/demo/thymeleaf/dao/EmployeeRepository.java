00b30bade665fed62a4cf0d53f3039418cd2f31a
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-29 21:51:38 +0100

    refactor(view): update employee list to be sorted by last name

--- src/main/java/com/example/demo/thymeleaf/dao/EmployeeRepository.java
+++ src/main/java/com/example/demo/thymeleaf/dao/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.thymeleaf.dao;
 
 import com.example.demo.thymeleaf.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it ... no need to write any code LOL!
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
