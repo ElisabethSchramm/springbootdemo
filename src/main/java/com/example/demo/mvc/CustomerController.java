46699c8967923d50b0dd3362e0a99d520fbdb2d2
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-10-29 13:52:12 +0100

    updated CustomerController: added trim to customer

--- src/main/java/com/example/demo/mvc/CustomerController.java
+++ src/main/java/com/example/demo/mvc/CustomerController.java
@@ -1,15 +1,24 @@
 package com.example.demo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        StringTrimmerEditor editor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, editor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model){
