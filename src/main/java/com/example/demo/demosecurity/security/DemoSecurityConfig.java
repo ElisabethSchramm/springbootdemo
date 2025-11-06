140eb26f0f464abef999f77ebb2b8b64ffcae782
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-06 22:56:32 +0100

    refactor(security): remove InMemoryUserDetailsManager in favor of database-backed authentication

--- src/main/java/com/example/demo/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/example/demo/demosecurity/security/DemoSecurityConfig.java
@@ -3,37 +3,20 @@ package com.example.demo.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails alex = User.builder()
-                .username("alex")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails paul = User.builder()
-                .username("paul")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails max = User.builder()
-                .username("max")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(alex, paul, max);
+    public UserDetailsManager detailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
