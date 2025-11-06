33a19c371769e82bb5e607fb7a23edabac88b5f8
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-06 23:41:01 +0100

    feat(security): configure JdbcUserDetailsManager with custom queries for members and roles tables

--- src/main/java/com/example/demo/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/example/demo/demosecurity/security/DemoSecurityConfig.java
@@ -13,10 +13,20 @@ import javax.sql.DataSource;
 public class DemoSecurityConfig {
 
     @Bean
-    public UserDetailsManager detailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
