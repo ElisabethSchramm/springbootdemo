6a90be153608ee0a20223e159b18cec580afa2a0
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-08 04:49:36 +0100

    feat(aop): restrict @Before advice in LoggingAspect to AccountDAO.addAccount

--- src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
@@ -11,7 +11,7 @@ public class LoggingAspect {
 
     // all of related advices for logging
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.example.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=====>>> Executing @Before advice on addAccount");
     }
