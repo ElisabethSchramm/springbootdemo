6e76e43922341d429b68de8118b13c9c6adc19b3
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-08 05:42:07 +0100

    feat(aop): update @Before advice to match all add* methods with any parameters

--- src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
@@ -11,7 +11,7 @@ public class LoggingAspect {
 
     // all of related advices for logging
 
-    @Before("execution(* add*(com.example.aopdemo.Account, ..))")
+    @Before("execution(* add*(..))")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=====>>> Executing @Before advice on addAccount");
     }
